3f3429cc8b1d7c9f51cf02b784a8b2ad1e887643
Filip <siposlaw95@gmail.com>
2022-02-10 18:50:18 +0100

    v1

---
+++ JS/putAfterAndBefore.js
@@ -0,0 +1,5 @@
+$(function (){
+    $('h2').before('<img src="../Graphic/img.png">');
+    $('img').after('<hr><br>');
+});
+//wstawianie przed i po znaczniku
---
+++ JS/secondexample.js
@@ -0,0 +1,4 @@
+$(function(){
+    $('a').odd().css({'color': 'white', 'text-decoration': 'none','background-color': 'black',})
+    $('a').not('#pierwszy').css({'color': 'white', 'text-decoration': 'none','background-color': 'black',})
+});
